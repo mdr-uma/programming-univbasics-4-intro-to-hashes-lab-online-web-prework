b0d107446c06ec56759272fb6c6044d34b833b07
helpful-animation-4719 <um_2003_8@hotmail.com>
2019-09-30 18:46:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,28 @@
 def new_hash
-  # return an empty hash
+  {}# return an empty hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  new_hash = {
+    :name => "Uma",
+    :age => 33
+  }# return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer ={
+    :name => "Grace Hopper"
+  }# return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  id_generator = {
+    :id => 5
+  }# return a hash with a key :id assigned to positive integer
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+ {key => value}# return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
